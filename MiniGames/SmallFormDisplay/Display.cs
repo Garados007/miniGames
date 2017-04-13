2f2df3d700346551dbe10578681db50eda660a2f
Max Brauer <ma.brauer@live.de>
2017-04-13 23:52:20 +0200

    Es wurde ein Display Plugin hinzugefügt, welches dann auch die Ersten Renderarbeiten übernehmen wird.

---
+++ MiniGames/SmallFormDisplay/Display.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MiniGamesInterface.Display;
+
+namespace SmallFormDisplay
+{
+    public partial class Display : Form
+    {
+        public Display()
+        {
+            InitializeComponent();
+        }
+
+        public void InvokeOrRun(Action action)
+        {
+            if (InvokeRequired) Invoke(action);
+            else action();
+        }
+    }
+
+    public class DisplayWorker : DisplayBase
+    {
+        public Display Display;
+
+        public override void Dispose()
+        {
+            Display?.InvokeOrRun(() => Display.Dispose());
+            Display = null;
+        }
+
+        public override void Hide()
+        {
+            Display?.InvokeOrRun(() => Display.Close());
+        }
+
+        public override void Show()
+        {
+            Display = new Display();
+            Application.Run(Display);
+        }
+    }
+
+    public class DisplayFactory : MiniGamesInterface.Display.DisplayFactory
+    {
+        public override string DisplaySymbolFile
+        {
+            get
+            {
+                return null;
+            }
+        }
+
+        public override string Name
+        {
+            get
+            {
+                return "Small Form Display";
+            }
+        }
+
+        public override DisplayBase CreateDisplay()
+        {
+            return new DisplayWorker();
+        }
+
+        public override LoadingMessageBase CreateLoadingMessage(string message)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override SpriteBase CreateSprite(string imagePath)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ MiniGames/SmallFormDisplay/Plugin.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MiniGamesInterface;
+using MiniGamesInterface.Display;
+
+namespace SmallFormDisplay
+{
+    public class Plugin : GraphicPluginBase
+    {
+        public Plugin()
+        {
+
+        }
+
+        public override string Description
+        {
+            get
+            {
+                return "Small and light Win Form";
+            }
+        }
+
+        public override string Name
+        {
+            get
+            {
+                return "Small Form Display Plugin";
+            }
+        }
+
+        public override MiniGamesInterface.Display.DisplayFactory[] GetAllDisplays()
+        {
+            return new MiniGamesInterface.Display.DisplayFactory[]
+            {
+                new DisplayFactory()
+            };
+        }
+
+
+    }
+}
