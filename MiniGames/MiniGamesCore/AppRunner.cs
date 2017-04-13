ff1ec01c15a49f7d72913658854386b9e608e00c
Max Brauer <ma.brauer@live.de>
2017-04-13 16:28:48 +0200

    Die Startklasse wurde hinzugefügt

--- MiniGames/MiniGamesCore/AppRunner.cs
+++ MiniGames/MiniGamesCore/AppRunner.cs
@@ -6,7 +6,11 @@ using System.Threading.Tasks;
 
 namespace MiniGamesCore
 {
-    public class Class1
+    public class AppRunner
     {
+        public static void Main(string[] args)
+        {
+
+        }
     }
 }
